872c938730cc9563c572bbbc027058590472bd6c
msajjauddin <msajjauddin@gmail.com>
2026-09-02 22:14:27 +0530

    changes in timeout api

--- modules/api/main.tf
+++ modules/api/main.tf
@@ -87,7 +87,7 @@ resource "aws_api_gateway_integration" "auth" {
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = "arn:aws:apigateway:${var.aws_region}:lambda:path/2015-03-31/functions/${var.auth_handler_arn}/invocations"
-  timeout_milliseconds    = 30000  # 30 seconds timeout
+  timeout_milliseconds    = 28000  # 30 seconds timeout
 }
 
 resource "aws_api_gateway_method_response" "auth" {
@@ -204,7 +204,7 @@ resource "aws_api_gateway_integration" "query" {
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = "arn:aws:apigateway:${var.aws_region}:lambda:path/2015-03-31/functions/${var.query_processor_arn}/invocations"
-  timeout_milliseconds    = 150000  # 2.5 minutes timeout
+  timeout_milliseconds    = 28000  # 2.5 minutes timeout
 }
 
 resource "aws_api_gateway_method_response" "query" {
@@ -262,7 +262,7 @@ resource "aws_api_gateway_integration" "upload" {
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = "arn:aws:apigateway:${var.aws_region}:lambda:path/2015-03-31/functions/${var.upload_handler_arn}/invocations"
-  timeout_milliseconds    = 150000  # 2.5 minutes timeout
+  timeout_milliseconds    = 28000  # 
 }
 
 resource "aws_api_gateway_method_response" "upload" {
